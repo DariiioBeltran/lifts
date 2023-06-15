974efcf6b3dbd4d35ebb0ee3c4e10260a07408de
DarioBeltran <dariiio.beltran@gmail.com>
2023-06-14 22:54:20 -0500

    FIRST_PASS: setting up skeleton

--- pr_tracking/monolith/pr_tracking_web/models/exercises.py
+++ pr_tracking/monolith/pr_tracking_web/models/exercises.py
@@ -1,7 +1,6 @@
 from django.db import models
-from .GymRat import GymRat
 from django.contrib.auth.models import User
 
 class Exercise(models.Model):
     gym_rat = models.ForeignKey(User, on_delete=models.CASCADE)
-    exercise = models.CharField(max_length=256)
+    exercise_name = models.CharField(max_length=128)
