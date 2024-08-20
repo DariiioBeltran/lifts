6cdf65b7c58489973e1ba6aa3b767def481e21a9
DariiioBeltran <dariiio.beltran@gmail.com>
2024-08-19 21:48:14 -0500

    records_table: oops

--- pr_tracking/monolith/swole_stats_api/views/record_views.py
+++ pr_tracking/monolith/swole_stats_api/views/record_views.py
@@ -1,7 +1,6 @@
 from swole_stats_api.serializers.record_serializers import WorkoutRecordSerializer
 from swole_stats_api.models.record_models import WorkoutRecord
 from rest_framework import generics
-from django.utils import timezone
 
 
 class WorkoutRecordListView(generics.ListCreateAPIView):
