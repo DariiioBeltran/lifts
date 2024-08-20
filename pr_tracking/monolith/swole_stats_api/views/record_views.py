bd85023599957c17ecdf7deeb6509ce170dcf752
DariiioBeltran <dariiio.beltran@gmail.com>
2024-08-19 21:47:29 -0500

    records_table: clean up

--- pr_tracking/monolith/swole_stats_api/views/record_views.py
+++ pr_tracking/monolith/swole_stats_api/views/record_views.py
@@ -3,13 +3,10 @@ from swole_stats_api.models.record_models import WorkoutRecord
 from rest_framework import generics
 from django.utils import timezone
 
-from rest_framework.permissions import IsAuthenticated, AllowAny
-
 
 class WorkoutRecordListView(generics.ListCreateAPIView):
     queryset = WorkoutRecord.objects.all().order_by("-id")[:14]
     serializer_class = WorkoutRecordSerializer
-    permission_classes = [AllowAny]
 
 
 class WorkoutRecordDetail(generics.RetrieveAPIView):
